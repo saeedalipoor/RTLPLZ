ef7f0d53359ecb156f6a34af0eb88ae36cac0651
Saeed Alipoor <alipoor.sa@gmail.com>
2019-09-12 05:42:19 +0430

    New updates
    Fix text wrap
    Update help

--- src/code.ts
+++ src/code.ts
@@ -4,22 +4,32 @@ figma.showUI(__html__, { width: 380, height: 280 });
 let text;
 const parabicNumbers = /[\u06f0-\u06f9]|[\u0660-\u0669]/g;
 const parabic = /[\u0600-\u08bd\s]|[\ufe70-\ufefc]/g;
-
+const superSubs = /[\u064b-\u065f]|[\u06D4-\u06DC]|[\u06DF-\u06E8]|[\u06EA-\u06ED]/g;
+Array.prototype['swapItems'] = function (a, b) {
+  this[a] = this.splice(b, 1, this[a])[0];
+  return this;
+}
 let node = figma.currentPage.selection[0];
 const revert = text => {
-  return text.split(' ').reduce((res, current) => {
+  return text.split(' ').reduce((res, word) => {
     const lastItem = res[res.length - 1];
-    const revertedCurrent = current.split('').reverse().join('');
-    if (current.trim().match(parabic) && !current.trim().match(parabicNumbers)) {
-      return res.concat(revertedCurrent);
+    const revertedCurrent = word.split('').reverse();
+    if (word.trim().match(parabic) && !word.trim().match(parabicNumbers)) {
+      if (word.match(superSubs)) {
+        word.match(superSubs).forEach(sym => {
+          revertedCurrent.swapItems(revertedCurrent.indexOf(sym), revertedCurrent.indexOf(sym) + 1);
+        });
+        return res.concat(revertedCurrent.join(''))
+      }
+      return res.concat(revertedCurrent.join(''));
     } else {
       if (!lastItem) {
-        return res.concat(current);
+        return res.concat(word);
       } else {
         if (!lastItem.replace(/ /g, '').match(parabic) || lastItem.match(parabicNumbers)) {
-          return res.slice(0, res.length - 1).concat(lastItem + ' ' + current);
-        }else{
-          return res.concat(current);
+          return res.slice(0, res.length - 1).concat(lastItem).concat(word);
+        } else {
+          return res.concat(word);
         }
       }
     }
@@ -32,24 +42,39 @@ if (node && node.type === 'TEXT') {
   figma.ui.postMessage({ type: 'init', msg: revertParagraph(node['characters']), nodeType: node.type });
 }
 figma.ui.onmessage = async msg => {
-  node = figma.currentPage.selection[0];
+  const { selection } = figma.currentPage;
+  node = selection[0];
   if (node && node.type === 'TEXT') {
     await figma.loadFontAsync(node.fontName as FontName);
     switch (msg.type) {
       case 'revert-text':
-        node['characters'] = revertParagraph(msg.text !== undefined ? msg.text : node['characters']);
-        if (msg.text === undefined) {
-          figma.ui.postMessage({ type: 'init', msg: revertParagraph(node['characters']) });
+        if (selection.length > 1 && !msg.single) {
+          selection.forEach(async node => {
+            if (node.type !== 'TEXT') return;
+            await figma.loadFontAsync(node.fontName as FontName);
+            node['characters'] = revertParagraph(msg.text !== undefined ? msg.text : node['characters']);
+            node['name'] = msg.text !== undefined ? msg.text : node['characters'];
+          })
+        } else {
+          node['characters'] = revertParagraph(msg.text !== undefined ? msg.text : node['characters']);
+          node['name'] = msg.text !== undefined ? msg.text : node['characters'];
+          if (msg.text === undefined) {
+            figma.ui.postMessage({ type: 'init', msg: revertParagraph(node['characters']) });
+          }
         }
         break;
-      case 'export-text':
-        if (msg.text) node['characters'] = revertParagraph(msg.text);
-        break;
-      case 'import-text':
-        figma.ui.postMessage({ type: 'init', msg: revertParagraph(node['characters']) });
-        break;
       case 'get-selected-layer':
-        figma.ui.postMessage({ type: 'selected-layer', msg: revertParagraph(node['characters']), notRtl: !node['characters'].replace(/ /g, '').match(parabic),nodeType: node.type });
+        figma.ui.postMessage({
+          type: 'selected-layer',
+          msg: revertParagraph(node['characters']),
+          notRtl: !node['characters'].replace(/ /g, '').match(parabic),
+          nodeType: node.type,
+          style: {
+            fontFamily: node.fontName['family'],
+            fontSize: node.fontSize,
+            width: node.width
+          }
+        });
         break;
       default:
         break;
--- src/ui.tsx
+++ src/ui.tsx
@@ -3,9 +3,12 @@ import * as ReactDOM from 'react-dom'
 import './ui.css'
 
 const defaultText = 'Type Here...';
-let editor;
+let editor, wrapCalc;
+const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
 const App = () => {
   const [selectedLayer, setSelectedLayer] = React.useState();
+  const [wrapperStyles, setWrapperStyles] = React.useState();
+  const [textIsToLarge, setTextIsToLarge] = React.useState();
 
   const SelectText = element => {
     if (!element)
@@ -23,10 +26,30 @@ const App = () => {
       e.stopPropagation();
       SelectText(e.target)
     }
-    if (e.keyCode === 82 && (e.metaKey || e.ctrlKey)) {
+    if (e.keyCode === 82 && (e.metaKey || e.ctrlKey)) { //ctrl|command + R
       parent.postMessage({ pluginMessage: { type: 'revert-text' } }, '*');
     }
-    
+    if (e.keyCode === 76 && (e.metaKey || e.ctrlKey)) { //ctrl|command + L
+      if (editor.innerText.length > 2000) {
+        setTextIsToLarge(true);
+      }
+      setTimeout(() => {
+        editor.innerHTML = editor.innerHTML.replace(/&nbsp;/g, ' ').replace(/<br>/g, ' ');
+        wrapCalc.innerHTML = editor.innerText.replace(/\r?\n|\r/g, ' ').split(' ').map(w => w.trim() && w !== '&nbsp;' ? '<span>' + w + '</span><span>&nbsp;</span>' : '<br />').join('');
+        editor.innerHTML = '';
+        let lastOffset = 0;
+        wrapCalc.childNodes.forEach(node => {
+          if (node.tagName === 'SPAN' && node.offsetTop > lastOffset) {
+            lastOffset = node.offsetTop;
+            editor.innerHTML = editor.innerHTML + '<br>';
+          }
+          editor.innerHTML = editor.innerHTML + (node.tagName === 'BR' ? '<br>' : node.innerHTML);
+        });
+        parent.postMessage({ pluginMessage: { type: 'revert-text', text: editor.innerText, single: true } }, '*');
+        setTextIsToLarge(false);
+      }, 100);
+    }
+
   }
   const onEditorChange = e => {
     parent.postMessage({ pluginMessage: { type: 'revert-text', text: e.target.innerText } }, '*');
@@ -43,13 +66,14 @@ const App = () => {
   }
   onmessage = (event) => {
     if (event.data.pluginMessage) {
+      setTextIsToLarge(false);
       switch (event.data.pluginMessage.type) {
         case 'init':
           updateEditor(event.data.pluginMessage.msg);
           setSelectedLayer(event.data.pluginMessage.msg);
+          setWrapperStyles(event.data.pluginMessage.style);
           break;
         case 'error':
-          console.log(event.data.pluginMessage.msg)
           switch (event.data.pluginMessage.code) {
             case 1001:
               setSelectedLayer(undefined);
@@ -65,6 +89,7 @@ const App = () => {
           updateEditor(event.data.pluginMessage.msg);
           // }
           setSelectedLayer(event.data.pluginMessage.msg);
+          setWrapperStyles(event.data.pluginMessage.style)
           break;
         default:
           break;
@@ -81,16 +106,55 @@ const App = () => {
   }
   return (
     <>
+      <div className="wrap-calc" ref={node => wrapCalc = node} style={wrapperStyles}></div>
       <div tabIndex={1} className="editor empty" contentEditable ref={node => editor = node} onKeyDown={onEditorKeyDown} onInput={onEditorChange} data-placeholder={selectedLayer !== undefined ? defaultText : ''}></div>
       {selectedLayer === undefined &&
         <div className="empty-message">
-          <div>
-            Select a text layer
-          </div>
-          <div style={{ fontSize: 17, margin: '10px 0 0', color: '#888' }}> then come back here
-          </div>
+          <div>Select text layer(s) to edit</div>
+          <div style={{ fontSize: 17, margin: '10px 0px 0px', color: '#888' }}> then come back here</div>
         </div>
       }
+      <div className={`hint ${selectedLayer !== undefined ? 'hidden' : ''}`}>
+        <div className="title">
+          Help
+        </div>
+        <div className="shortcut">
+          <span className="desc">Revert Selected Layer(s)</span>
+          <span className="key-comb">
+            <span className="key">
+              {isMac ?
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3H6a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3V6a3 3 0 0 0-3-3 3 3 0 0 0-3 3 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path></svg>
+                :
+                `Ctrl`
+              }
+            </span>
+            <span className="key">
+              R
+            </span>
+          </span>
+        </div>
+        <div className="shortcut">
+          <span className="desc">Fix Selected Text Wrapping</span>
+          <span className="key-comb">
+            <span className="key">
+              {isMac ?
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3H6a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3V6a3 3 0 0 0-3-3 3 3 0 0 0-3 3 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 3 3 0 0 0-3-3z"></path></svg>
+                :
+                `Ctrl`
+              }
+            </span>
+            <span className="key">
+              L
+            </span>
+          </span>
+        </div>
+      </div>
+      {textIsToLarge && (
+        <div className="empty-message">
+          More than 2K characters<br />
+          Stand up and do some exercise.
+        </div>
+      )}
     </>
   )
 }
