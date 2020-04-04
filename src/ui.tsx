6b284d3f0bf6390c63fe3a5f612dbafd016b4982
Saeed Alipoor <alipoor.sa@gmail.com>
2020-04-04 11:25:43 +0430

    Upadte plugin ui

--- src/ui.tsx
+++ src/ui.tsx
@@ -5,118 +5,63 @@ import './ui.css'
 const defaultText = 'Type Here...';
 let editor, wrapCalc;
 const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+
 const App = () => {
-  const [selectedLayer, setSelectedLayer] = React.useState();
-  const [wrapperStyles, setWrapperStyles] = React.useState();
-  const [textIsToLarge, setTextIsToLarge] = React.useState();
-
-  const SelectText = element => {
-    if (!element)
-      return;
-    var range, selection;
-    selection = window.getSelection();
-    range = document.createRange();
-    range.selectNodeContents(element);
-    selection.removeAllRanges();
-    selection.addRange(range);
-  }
-  const onEditorKeyDown = e => {
-    if (e.keyCode === 65 && e.metaKey) {
-      e.preventDefault();
-      e.stopPropagation();
-      SelectText(e.target)
+  const [focused, setFocused] = React.useState(false);
+  const [selectionCount, setSelectionCount] = React.useState(null);
+  const editor = React.useRef(null);
+  const setText = txt => editor.current.innerText = txt;
+  onmessage = ({ data: { pluginMessage: data } }) => {
+    switch (data.type) {
+      case 'text':
+        if (!focused && editor.current && editor.current.innerText !== data.msg) {
+          setText(data.msg);
+          // editor.current.focus();
+          // document.execCommand('selectAll', false, null);
+          // document.getSelection().collapseToEnd();
+        }
+        break;
+      case 'selectionCount':
+        if (data.msg !== 1) {
+          setText('');
+        }
+        setSelectionCount(data.msg)
+        break;
+      default:
+        break;
     }
-    if (e.keyCode === 82 && (e.metaKey || e.ctrlKey)) { //ctrl|command + R
-      parent.postMessage({ pluginMessage: { type: 'revert-text' } }, '*');
-    }
-    if (e.keyCode === 76 && (e.metaKey || e.ctrlKey)) { //ctrl|command + L
-      if (editor.innerText.length > 2000) {
-        setTextIsToLarge(true);
-      }
-      setTimeout(() => {
-        editor.innerHTML = editor.innerHTML.replace(/&nbsp;/g, ' ').replace(/<br>/g, ' ');
-        wrapCalc.innerHTML = editor.innerText.replace(/\r?\n|\r/g, ' ').split(' ').map(w => w.trim() && w !== '&nbsp;' ? '<span>' + w + '</span><span>&nbsp;</span>' : '<br />').join('');
-        editor.innerHTML = '';
-        let lastOffset = 0;
-        wrapCalc.childNodes.forEach(node => {
-          if (node.tagName === 'SPAN' && node.offsetTop > lastOffset) {
-            lastOffset = node.offsetTop;
-            editor.innerHTML = editor.innerHTML + '<br>';
-          }
-          editor.innerHTML = editor.innerHTML + (node.tagName === 'BR' ? '<br>' : node.innerHTML);
-        });
-        parent.postMessage({ pluginMessage: { type: 'revert-text', text: editor.innerText, single: true } }, '*');
-        setTextIsToLarge(false);
-      }, 100);
-    }
-
   }
   const onEditorChange = e => {
-    parent.postMessage({ pluginMessage: { type: 'revert-text', text: e.target.innerText.replace(/\n$/, '') } }, '*');
-    if (e.keyCode === 65 && e.metaKey) {
-      e.preventDefault();
-      e.stopPropagation();
-      SelectText(e.target)
-    }
-    editor.classList[!e.target.innerText ? 'add' : 'remove']('empty');
-  }
-  const updateEditor = text => {
-    editor.classList[!text ? 'add' : 'remove']('empty');
-    editor.innerText = text;
-  }
-  onmessage = (event) => {
-    if (event.data.pluginMessage) {
-      setTextIsToLarge(false);
-      switch (event.data.pluginMessage.type) {
-        case 'init':
-          updateEditor(event.data.pluginMessage.msg);
-          setSelectedLayer(event.data.pluginMessage.msg);
-          setWrapperStyles(event.data.pluginMessage.style);
-          break;
-        case 'error':
-          switch (event.data.pluginMessage.code) {
-            case 1001:
-              setSelectedLayer(undefined);
-              updateEditor('');
-              break;
-
-            default:
-              break;
-          }
-          break;
-        case 'selected-layer':
-          // if (!editor.innerText) {
-          updateEditor(event.data.pluginMessage.msg);
-          // }
-          setSelectedLayer(event.data.pluginMessage.msg);
-          setWrapperStyles(event.data.pluginMessage.style)
-          break;
-        default:
-          break;
-      }
-    }
-  }
-  window.onload = () => {
-    if (editor) {
-      editor.focus();
-    }
-  }
-  window.onfocus = () => {
-    parent.postMessage({ pluginMessage: { type: 'get-selected-layer' } }, '*');
+    parent.postMessage({ pluginMessage: { type: 'input', payload: e.target.innerText.replace(/\n$/, '') } }, '*');
   }
   return (
     <>
-      <div className="wrap-calc" ref={node => wrapCalc = node} style={wrapperStyles}></div>
-      <div tabIndex={1} className="editor empty" contentEditable ref={node => editor = node} onKeyDown={onEditorKeyDown} onInput={onEditorChange} data-placeholder={selectedLayer !== undefined ? defaultText : ''}></div>
-      {selectedLayer === undefined &&
+      <div
+        tabIndex={1}
+        className="editor empty"
+        contentEditable={selectionCount === 1}
+        ref={editor}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        onInput={onEditorChange}
+        onPasteCapture={e => {
+          e.preventDefault();
+          var text = e.clipboardData.getData('text/plain');
+          document.execCommand("insertHTML", false, text);
+        }}
+      >
+      </div>
+      {selectionCount !== null && selectionCount !== 1 &&
         <div className="empty-message">
-          <div>Select text layer(s) to edit</div>
-          <div style={{ fontSize: 17, margin: '10px 0px 0px', color: '#888' }}> then come back here</div>
+          <div>Select {selectionCount === 0 ? 'a' : <strong>only one</strong>} text layer to edit</div>
         </div>
       }
-      <div className={`hint ${selectedLayer !== undefined ? 'hidden' : ''}`}>
+      {/* <div className={`hint ${selectionCount === 1 ? 'hidden' : ''}`}>
         <div className="title">
           Help
+        </div>
+        <div>
+          Shortcuts are available only when this window is focused.
         </div>
         <div className="shortcut">
           <span className="desc">Revert Selected Layer(s)</span>
@@ -134,7 +79,7 @@ const App = () => {
           </span>
         </div>
         <div className="shortcut">
-          <span className="desc">Fix Selected Text Wrapping</span>
+          <span className="desc">Fix Text Wrapping</span>
           <span className="key-comb">
             <span className="key">
               {isMac ?
@@ -148,13 +93,7 @@ const App = () => {
             </span>
           </span>
         </div>
-      </div>
-      {textIsToLarge && (
-        <div className="empty-message">
-          More than 2K characters<br />
-          Stand up and do some exercise.
-        </div>
-      )}
+      </div> */}
     </>
   )
 }
